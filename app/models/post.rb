4c6da69e2111333195f4714b7924fe39b1e983c4
prerit <preritmisra@hotmail.com>
2018-01-22 09:57:37 +0530

    rake db:migrate VERSION=0, reset DB since users not unique.Functionality now seems to be working

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,33 +4,9 @@ class Post < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 100} # Questions are capped at 100 chars.
-  validates :post_is_unique,presence: true
-  validates :post_order, presence: true
-  scope :ordered_by_vote, -> { order(cached_votes_score: :DESC) }
   default_scope { order(cached_votes_score: :DESC) }
 
-  def post_order 
-  
-  end
+ 
 
-  private
   
-    def post_is_unique
-      # Select all posts where the id does not match current id and fetch the contents
-      other_post_contents = self.class.where.not(id: id).pluck(:content)
-      # Split each content by space (**NOTE:** Other data cleaning may be needed -- remove non-alphanumeric characters)
-      other_post_contents = other_post_contents.map{ |c| c.split(" ").uniq }
-      # Now we need to compare the current content with all the other contents and see if they match
-      current_content = content.split(" ").uniq
-      other_post_contents.each do |other_content|
-        # words in common
-        common_words = other_content & current_content
-        # If the ratio of common words exceeds 0.8 (80%)
-        if (common_words.length / other_content.length) > 0.7
-          # Add an error and exit early
-          errors.add(:base, "current review is too similar to another review")
-          return false
-        end
-      end
-    end
 end
